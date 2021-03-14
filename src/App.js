55ba0f64f730061a46788bca3697d4126cd623c8
meladsamuel <meladsamuel2@gmail.com>
2021-03-14 21:11:44 +0200

    test eslint and prettier if work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import "./App.css";
-import React from "react";
+import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [msg, setMsg] = useState(null);
+  function hiHandler() {
+    setMsg('hi');
+  }
   return (
     <div className="App">
       <header className="App-header">
+        <p>{msg}</p>
         <p>welcome abdelrazek and mostafa :)</p>
+        <p> test the deploy from other repo</p>
+        <button type="button" onClick={hiHandler}>
+          hi
+        </button>
       </header>
     </div>
   );
