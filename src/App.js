e0d1649e5a6131d792ffb5c02ef0e75ae46b4b39
meladsamuel <meladsamuel2@gmail.com>
2021-03-14 15:27:13 +0200

    welcome to mosafa

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import "./App.css";
 import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>welcome abdelrazek</p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>welcome abdelrazek and mostafa :)</p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
